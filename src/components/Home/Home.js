da28832f9bc17b41df8b655eb7fb02c37e097bec
Asif Mohammad <asifratul45@gmail.com>
2021-04-08 00:28:52 +0600

    added react-router

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Shared/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            This is home
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Home;
