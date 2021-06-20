8e075b1097c9300d5e683eb38965dfb940e50d6e
Asif Mohammad <asifratul45@gmail.com>
2021-06-21 00:44:46 +0600

    load spinner added in travel group section

--- src/components/Home/HighlightedTravelGroup.js
+++ src/components/Home/HighlightedTravelGroup.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './HighlightedTravelGroup.css';
 
 const HighlightedTravelGroup = ({group}) => {
     return (
-        <div className="col-md-2 text-center my-4 mx-4">
+        <div className="highlighted-travel-group-container col-md-2 text-center my-4 mx-4">
             <img className="logo" src={`data:image/png;base64,${group.logo.img}`} alt=""/>
         </div>
     );
--- src/components/Home/TravelGroupSection.js
+++ src/components/Home/TravelGroupSection.js
@@ -8,23 +8,31 @@ const TravelGroupSection = () => {
     const {groupList} = useContext(UserContext);
 
     return (
-        <div style={{position: "relative"}}>
+        <div className="travel-group-container" style={{position: "relative"}}>
             <div className="travel-group-parallax"></div>
             <div className="travelGroupSection">
-                <h1 className="text-center text-white pt-2">Travel Groups</h1>
+                <div className="mx-auto text-center text-white">
+                    <h1 className="mt-3 mb-5">Travel Groups</h1>
 
-                <div className="row mx-0 justify-content-center">
                     {
-                        groupList.map(group => <HighlightedTravelGroup key={group._id} group={group} />)
+                        groupList.length
+                        ?
+                        <div className="row justify-content-center">
+                            {
+                                groupList.map(group => <HighlightedTravelGroup key={group._id} group={group} />)
+                            }
+                        </div>
+                        :
+                        <div className="loader mx-auto"></div>
                     }
-                </div>
 
-                <div className="container d-flex justify-content-center pb-4">
-                    <Link to="/travelGroupList">
-                        <button className="btn btn-lg see-more-button">
-                            See More
-                        </button>
-                    </Link>
+                    <div className="container d-flex justify-content-center pb-4">
+                        <Link to="/travelGroupList">
+                            <button className="btn btn-lg see-more-button">
+                                See More
+                            </button>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
