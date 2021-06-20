f06ad8c0a04bf82bb669977bb5d07627551551f7
Asif Mohammad <asifratul45@gmail.com>
2021-06-21 00:53:41 +0600

    load spinner added in travel group section

--- src/components/DestinationList/DestinationList.js
+++ src/components/DestinationList/DestinationList.js
@@ -2,19 +2,26 @@ import React from 'react';
 import { useContext } from 'react';
 import { UserContext } from '../../App';
 import DestinationlistItem from './DestinationListItem';
+import './DestinationList.css';
 
 const DestinationList = () => {
     const {destinationList} = useContext(UserContext);
 
     return (
-        <div className="container">
+        <div className="destination-list container">
             <h1 className="text-center mb-5">Travel Destinations</h1>
 
-            <div className="row">
-                {
-                    destinationList.map(destination => <DestinationlistItem key={destination._id} destination={destination} />)
-                }
-            </div>
+            {
+                destinationList.length
+                ?
+                <div className="row">
+                    {
+                        destinationList.map(destination => <DestinationlistItem key={destination._id} destination={destination} />)
+                    }
+                </div>
+                :
+                <div className="loader mx-auto"></div>
+            }
         </div>
     );
 };
--- src/components/TravelGroupList/TravelGroupList.js
+++ src/components/TravelGroupList/TravelGroupList.js
@@ -1,19 +1,26 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../../App';
 import GroupListItem from './GroupListItem';
+import './TravelGroupList.css';
 
 const TravelGroupList = () => {
     const {groupList} = useContext(UserContext);
 
     return (
-        <div className="container">
-            <h1 className="text-center mb-5">Travel Groups</h1>
+        <div className="travel-group-list container">
+            <h1 className="text-center my-5">Travel Groups</h1>
 
-            <div className="row">
-                {
-                    groupList.map(group => <GroupListItem key={group._id} group={group} />)
-                }
-            </div>
+            {
+                groupList.length
+                ?
+                <div className="row">
+                    {
+                        groupList.map(group => <GroupListItem key={group._id} group={group} />)
+                    }
+                </div>
+                :
+                <div className="loader mx-auto"></div>
+            }
         </div>
     );
 };
